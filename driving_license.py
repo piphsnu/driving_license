e0776e94b8f4c5e6b6d1ce62afdd78c24eccdf04
Joseph Chen <piphsnu@gmail.com>
2018-09-16 19:34:17 +0800

    Add USA condition

--- driving_license.py
+++ driving_license.py
@@ -1,8 +1,13 @@
 nat = input ('please input your nationality (Taiwan/USA):')
 age = input ('please input your age:')
 age = int (age)  # casting from string to integer
-if nat == 'Taiwan':
+if nat == 'Taiwan': # setup Taiwan condition
 	if age >= 18:
+		print ('You can apply for driving license exam')
+	else:
+		print ('You cannot apply for driving license exam')
+if nat == 'USA': # setup USA condition
+	if age >= 16:
 		print ('You can apply for driving license exam')
 	else:
 		print ('You cannot apply for driving license exam')
